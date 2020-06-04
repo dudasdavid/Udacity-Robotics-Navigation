20b655d9b7f81b0cdda93c49e1ef7bb4db7bc4ca
David Dudas <david.dudas@outlook.com>
2020-06-05 01:00:21 +0200

    add navigation testing bash script

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# build the workspace
+cd $(pwd)/../../..; catkin_make
+
+source devel/setup.bash
+source /opt/ros/kinetic/setup.bash
+export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/Udacity-Robotics-Navigation/worlds/MyWorld.world"
+export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/Udacity-Robotics-Navigation/map/map.yaml"
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &    
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" & 
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
