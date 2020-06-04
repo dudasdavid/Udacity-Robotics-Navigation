1b87e5eb30c23fcb068c98f18b8b85e5802586c4
David Dudas <david.dudas@outlook.com>
2020-06-05 00:33:43 +0200

    change sh to bash

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
-sleep 5
-xterm  -e  " source ../../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &    
+#!/bin/bash
+
+# build the workspace
+cd $(pwd)/../../..; catkin_make
+
+source devel/setup.bash
+source /opt/ros/kinetic/setup.bash
+export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/Udacity-Robotics-Navigation/worlds/MyWorld.world"
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &    
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
 sleep 5
